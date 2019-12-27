d8cf9fed4a2b6ff3b9b569fe11f435913404654a
LeThanhTung <thanhtung5598@gmail.com>
2019-12-27 17:22:24 +0700

    Filter Name

--- index.js
+++ index.js
@@ -6,16 +6,28 @@ const port = 3000;
 app.set('view engine', 'pug')
 app.set('views', './views') // Started with file views
 
+users =  [
+    { id: 1, name: 'thanh' },
+    { id: 2, name: 'Linh' }
+]
+
 app.get('/', (req, res) => {
     res.render('index');
 })
 
 app.get('/user', (req, res) => {
     res.render('users/user', {
-        users: [
-            { id: 1, name: 'thanh' },
-            { id: 2, name: 'Linh' }
-        ]
+        users
+    })
+})
+
+app.get('/user/search',(req,res)=>{
+    let name = req.query.name;
+    let matchName = users.filter((user)=>{
+        return user.name.indexOf(name) !== -1;
+    })
+    res.render('users/user', {
+        users:matchName
     })
 })
 
