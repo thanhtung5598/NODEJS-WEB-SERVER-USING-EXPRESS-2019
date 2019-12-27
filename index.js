27a05d65aacd25a84f2034666c28289f341aa7a2
LeThanhTung <thanhtung5598@gmail.com>
2019-12-27 19:24:10 +0700

    View User

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const shortid = require('shortid');
 // Lowdb Start
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
@@ -18,7 +18,8 @@ app.set('view engine', 'pug')
 app.set('views', './views') // Started with file views
 
 // Set some defaults (required if your JSON file is empty)
-db.defaults({ users: [] }).write();
+db.defaults({ users: [] })
+    .write();
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -26,28 +27,39 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
     res.render('users/user', {
-        users:db.get('users').value()
+        users: db.get('users').value()
     })
 })
 
-app.get('/users/search',(req,res)=>{
+app.get('/users/search', (req, res) => {
     let name = req.query.name;
-    let matchName = db.get('users').value().filter((user)=>{
+    let matchName = db.get('users').value().filter((user) => {
         return user.name.indexOf(name) !== -1;
     })
     res.render('users/user', {
-        users:matchName
+        users: matchName
     })
 })
 
-app.get('/users/create',(req,res)=>{
+app.get('/users/create', (req, res) => {
     res.render('users/create');
 })
 
-app.post('/users/create',(req,res)=>{
-    db.get('users').push(req.body).write();
-    res.redirect('/users')
+app.get('/users/:id', (req, res) => {
+    let id = req.params.id;
+    let user = db.get('users').find({ id: id }).value();
+    res.render('users/view', {
+        user: user
+    })
 })
+
+app.post('/users/create', (req, res) => {
+    req.body.id=shortid.generate()
+    db.get('users').push(req.body).write();
+    res.redirect('/users');
+})
+
+
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
